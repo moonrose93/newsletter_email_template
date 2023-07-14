5dd92b651c1a82be5fbb465f647d23c4cff10a3c
Isidora Stanisavljevska <137770804+moonrose93@users.noreply.github.com>
2023-07-14 13:17:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let popup = document.getElementById("popup");
+const notValid = document.querySelector('.not-valid');
+const wrapper = document.querySelector('.wrapper');
+
+function openPopup() {
+  var emailInput = document.querySelector('input[type="text"]').value;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(emailInput)) {
+    popup.classList.add("open-popup");
+    wrapper.classList.add('none-active');
+    wrapper.classList.remove("wrapper");
+    
+  } else {
+    notValid.classList.add('active');
+    notValid.classList.remove('not-valid');
+  }
+}
+
+function closePopup() {
+  popup.classList.remove("open-popup");
+}
+
+function hideWrapper() {
+  var emailInput = document.querySelector('input[type="text"]').value;
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  if (emailRegex.test(emailInput)) {
+    document.querySelector('.wrapper').style.display = "none";
+  }
+}
+
+function displayErrorMessage(message) {
+  var errorContainer = document.getElementById("error-container");
+  var errorMessage = document.createElement("p");
+  errorMessage.className = "error-message";
+  errorMessage.textContent = message;
+  errorContainer.appendChild(errorMessage);
+}
+
+
+
